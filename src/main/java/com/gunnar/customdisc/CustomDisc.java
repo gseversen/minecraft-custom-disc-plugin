820ef50a744fe7fb0919285c3bf70c9e4d86a75e
gseversen <geversenn@gmail.com>
2026-01-20 09:42:49 -0600

    Test phase, jar file created to be tested on the server with a cat sound.

--- src/main/java/com/gunnar/customdisc/CustomDisc.java
+++ src/main/java/com/gunnar/customdisc/CustomDisc.java
@@ -1,5 +1,9 @@
 package com.gunnar.customdisc;
 
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CustomDisc extends JavaPlugin {
@@ -10,8 +14,21 @@ public class CustomDisc extends JavaPlugin {
     }
 
     @Override
-    public void onDisable() {
-        getLogger().info("CustomDisc plugin stopped!");
-        // Your plugin cleanup code here
-    }    
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        
+        if(!(sender instanceof Player player)) {
+            sender.sendMessage("Only players can use this command.");
+            return true;
+        }
+
+        player.playSound(player.getLocation(), Sound.MUSIC_DISC_CAT, 1.0f, 1.0f);
+        player.sendMessage("Playing custom disc sound!");
+        return true;
+    }
+
+    // @Override
+    // public void onDisable() {
+    //     getLogger().info("CustomDisc plugin stopped!");
+    //     // Your plugin cleanup code here
+    // }    
 }
